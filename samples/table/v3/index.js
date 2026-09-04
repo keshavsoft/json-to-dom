82daea47ff2ed8000b86f5de276dbe61835814d3
keshavsoft <nknnkumar@yahoo.com>
2026-09-04 14:34:44 +0530

    table

--- samples/table/v3/index.js
+++ samples/table/v3/index.js
@@ -1,9 +1,8 @@
-import data from "./data.json" with { type: "json" };
-import { generateTableSpec } from "./generateTableSpec.js";
+import spec from "./spec.json" with { type: "json" };
 import { renderTable } from "./renderTable.js";
 
 const startFunc = () => {
-    const tableSpec = generateTableSpec({ inData: data });
+    const tableSpec = spec;
     renderTable({ inSpec: tableSpec, inTargetContainerId: "table-container" });
 };
 
