35d47fef4c41825a2e271e3a44e6e886d85682d0
keshavsoft <nknnkumar@yahoo.com>
2026-09-05 11:39:12 +0530

    rend 5

---
+++ samples/hybrid/v2/index.js
@@ -0,0 +1,63 @@
+import columns from "./columns.json" with { type: "json" };
+import data from "./data.json" with { type: "json" };
+import tableConfig from "./table/config.json" with { type: "json" };
+import { Table } from "../../../renderers/v5/table/Table.js";
+
+import searchConfig from "./search/config.json" with { type: "json" };
+import { Form } from "../../../renderers/v5/form/Form.js";
+import { DataList } from "../../../renderers/v5/datalist/DataList.js";
+
+const startFunc = () => {
+    // 1. Instantiate Table with clean public API
+    const table = new Table({
+        inData: data,
+        inColumns: columns,
+        inConfig: tableConfig,
+        inTargetContainerId: "table-container"
+    });
+
+    const tableControlsTree = table.render();
+
+    // 2. Instantiate and render DataList for autocomplete with counts
+    const dataList = new DataList({
+        inData: data,
+        inColumns: columns,
+        inTargetContainerId: "datalist-container"
+    });
+
+    dataList.render();
+
+    console.log("dataList : ", dataList);
+
+    // 3. Instantiate and render Form
+    const form = new Form({
+        inColumns: columns,
+        inConfig: searchConfig,
+        inTargetContainerId: "filter-container"
+    });
+
+    const fromForm = form.render();
+
+    const formElement = fromForm.element;
+
+    const buttons = formElement.querySelectorAll("button");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", event => {
+            const currentTarget = event.currentTarget;
+            const closestRow = currentTarget.closest("div");
+            const input = closestRow.querySelector("input");
+            const name = input.getAttribute("name");
+            const value = input.value;
+            const query = {};
+            query[name] = value;
+
+            table.filterStateData({ inQuery: query });
+
+            // Update datalist autocomplete options with new filtered state counts
+            dataList.update({ inData: table.store.stateData });
+        });
+    });
+};
+
+startFunc();
