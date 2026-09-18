ad925817c1fcabcbdcebcc122a83407e9ebb726b
keshavsoft <nknnkumar@yahoo.com>
2026-09-18 11:05:51 +0530

    src 35

--- samples/row/v14/index.js
+++ samples/row/v14/index.js
@@ -1,13 +1,32 @@
+import spec from "./spec.json" with { type: "json" };
+import data from "./data.json" with { type: "json" };
+
 const startFunc = async () => {
     try {
-        const spec = await fetch("./spec.json").then(res => res.json());
+        // const spec = await fetch("./spec.json").then(res => res.json());
 
         // 1. Output 1: Live DOM Node
-        window.ks['json-to-dom'].buildSpecElement({
+        const k1 = window.ks['json-to-dom'].buildSpecElement({
             spec,
             domIdToPushTo: "form-container",
-            outputType: "spec"
+            output: { type: "spec", data }
         });
+
+        const k2 = window.ks['json-to-dom'].buildSpecElement({
+            spec: k1,
+            domIdToPushTo: "form-container",
+            output: { type: "dom" }
+        });
+
+
+        console.log("kkkkkkkkkk : ", k1);
+
+        // window.ks['json-to-dom'].buildSpecElement({
+        //     spec,
+        //     domIdToPushTo: "form-container",
+        //     output: { type: "dom" }
+        // });
+
     } catch (err) {
         console.error("Failed to render v27 sample:", err);
     }
---
+++ src/v35/meta.js
@@ -0,0 +1,6 @@
+export const meta = {
+    version: "v33.0",
+    description: "Pure DOM engine (v33)"
+};
+
+export default meta;
