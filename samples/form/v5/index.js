5ab3cb82277f93c5891191828e6fba801e4d4c58
keshavsoft <nknnkumar@yahoo.com>
2026-09-20 10:25:58 +0530

    jsdom to test

--- samples/form/v5/index.js
+++ samples/form/v5/index.js
@@ -1,20 +1,27 @@
 import { buildSpec } from "../../../src/v40/index.js";
+import { JSDOM } from "jsdom";
+import spec from "./spec.json" with { type: "json" };
+const { window } = new JSDOM();
+
+const document = window.document;
+
+globalThis.document = document;
 
 const startFunc = async () => {
     try {
-        const spec = await fetch("./spec.json").then(res => res.json());
-
         // 1. Output 1: Live DOM Node
         const k1 = buildSpec({
             inSpec: spec
         });
         console.log("k1: ", k1[0]);
 
-        const c1 = document.getElementById("form-container");
-        c1.appendChild(k1[0]);
+        console.log(k1.outerHTML);
+
+        // const c1 = document.getElementById("form-container");
+        // c1.appendChild(k1[0]);
     } catch (err) {
         console.error("Failed to render v27 sample:", err);
     }
 };
 
-startFunc();
+startFunc().then();
